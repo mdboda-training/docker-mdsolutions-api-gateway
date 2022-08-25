e84f61e79f19322582ae04bb7dc53e270add8a58
NAVEEN <boda.gadhilinga@gmail.com>
2022-08-25 18:45:55 +0530

    Initial Commit

---
+++ src/main/java/com/mdsolutions/api/gateway/MdsolutionsApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.mdsolutions.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdsolutionsApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdsolutionsApiGatewayApplication.class, args);
+	}
+
+}
